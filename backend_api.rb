04d33fa5c5753cede81d15572dcc73f0deb5afb6
Daniel Del Rio <danieldr@ens-labs.com>
2014-12-11 10:34:34 -0400

    Fixed pueblo_especifico

--- backend_api.rb
+++ backend_api.rb
@@ -24,24 +24,27 @@ class AEE_API
 		
 		hash_table = breakdownstuff.body
 		hash_pueblo = Hash.new
+		hash_averia = Hash.new
+		hash_array_averias = Array.new
 
 		hash_table.each do |key, value|
 			cantidad_averias_pueblo = value[:return].length
 			# Checks if its an array of hashes or a hash
 			if value[:return].kind_of?(Array)
-				hash_hash = Hash.new
 				for averias in 1..cantidad_averias_pueblo
 					averias -= 1
-					hash_hash["Area: "] = value[:return][averias][:r2_area]
-					hash_hash["Status: "] = value[:return][averias][:r3_status]
-					hash_hash["Last Update: "] = value[:return][averias][:r4_last_update]
-					hash_pueblo["#{value[:return][averias][:r1_town_or_city]}"] = hash_hash
+					hash_averias = Hash.new
+					hash_averias["Area:"] = value[:return][averias][:r2_area]
+					hash_averias["Status:"] = value[:return][averias][:r3_status]
+					hash_averias["Last Update:"] = value[:return][averias][:r4_last_update]
+					hash_array_averias[averias] = hash_averias
 				end
+				hash_pueblo["#{value[:return][averias][:r1_town_or_city]}"] = hash_array_averias
 			else
-				hash_hash["Area: "] = value[:return][:r2_area]
-				hash_hash["Status: "] = value[:return][:r3_status]
-				hash_hash["Last Update: "] = value[:return][:r4_last_update]
-				hash_pueblo["#{value[:return][:r1_town_or_city]}"] = hash_hash
+				hash_averia["Area:"] = value[:return][:r2_area]
+				hash_averia["Status:"] = value[:return][:r3_status]
+				hash_averia["Last Update:"] = value[:return][:r4_last_update]
+				hash_pueblo["#{value[:return][:r1_town_or_city]}"] = hash_averia
 			end
 		end
 		return hash_pueblo.to_json
