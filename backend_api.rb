054739c2bc6725b389999b3cbdcb43628376d183
Daniel Del Rio <danieldr@ens-labs.com>
2014-12-11 15:05:19 -0400

    Done fixing and organizing

--- backend_api.rb
+++ backend_api.rb
@@ -34,16 +34,16 @@ class AEE_API
 				for averias in 1..cantidad_averias_pueblo
 					averias -= 1
 					hash_averias = Hash.new
-					hash_averias["Area:"] = value[:return][averias][:r2_area]
-					hash_averias["Status:"] = value[:return][averias][:r3_status]
-					hash_averias["Last Update:"] = value[:return][averias][:r4_last_update]
+					hash_averias["Area"] = value[:return][averias][:r2_area]
+					hash_averias["Status"] = value[:return][averias][:r3_status]
+					hash_averias["Last Update"] = value[:return][averias][:r4_last_update]
 					hash_array_averias[averias] = hash_averias
 				end
 				hash_pueblo["#{value[:return][averias][:r1_town_or_city]}"] = hash_array_averias
 			else
-				hash_averia["Area:"] = value[:return][:r2_area]
-				hash_averia["Status:"] = value[:return][:r3_status]
-				hash_averia["Last Update:"] = value[:return][:r4_last_update]
+				hash_averia["Area"] = value[:return][:r2_area]
+				hash_averia["Statu:"] = value[:return][:r3_status]
+				hash_averia["Last Update"] = value[:return][:r4_last_update]
 				hash_pueblo["#{value[:return][:r1_town_or_city]}"] = hash_averia
 			end
 		end
@@ -54,7 +54,6 @@ class AEE_API
 		aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
 		pueblos = Array.new
 		hash_pueblo = Hash.new
-		hash_averia = Hash.new
 
 		aee_client = Savon.client(wsdl: aee_url)
 		breakdownSummary = aee_client.call(:get_breakdowns_summary)
@@ -73,18 +72,22 @@ class AEE_API
 			data = breakdownstuff.body
 			if data[:get_breakdowns_by_town_or_city_response][:return].kind_of?(Array)
 				cantidad_averias_pueblo = data[:get_breakdowns_by_town_or_city_response][:return].length
-				hash_averia = Array.new
+				array_averias = Array.new
 				for averias in 1..cantidad_averias_pueblo
 					averias -= 1
 					hash_averias = Hash.new
-					hash_averias["Area:"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r2_area]
-					hash_averias["Status:"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r3_status]
-					hash_averias["Last Update:"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r4_last_update]
-					hash_averia[averias] = hash_averias 
+					hash_averias["Area"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r2_area]
+					hash_averias["Status"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r3_status]
+					hash_averias["Last Update"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r4_last_update]
+					array_averias[averias] = hash_averias 
 				end
-				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][averias][:r1_town_or_city]}"] = hash_averia
+				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][averias][:r1_town_or_city]}"] = array_averias
 			else
-				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]}"] = data[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
+				hash_averia = Hash.new
+				hash_averia["Area"] = data[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
+				hash_averia["Status"] = data[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
+				hash_averia["Last Update"] = data[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
+				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]}"] = hash_averia
 			end
 		end
 		return hash_pueblo.to_json
