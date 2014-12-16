fb3036baeba951713c17b6896d97158f1d439ecf
Daniel Del Rio <danieldr@ens-labs.com>
2014-12-15 22:50:10 -0400

    organizing data

--- backend_api.rb
+++ backend_api.rb
@@ -26,13 +26,14 @@ class AEE_API
 		breakdownstuff = aee_client.call(:get_breakdowns_by_town_or_city, message: { "townOrCity" => pueblito.upcase })
 		
 		hash_table = breakdownstuff.body
-		hash_pueblo = Hash.new
+		array_final = Array.new
 
 		hash_table.each do |key, value|
 			cantidad_averias_pueblo = value[:return].length
 			# Checks if its an array of hashes or a hash
 			if value[:return].kind_of?(Array)
 				hash_array_averias = Array.new
+				hash_pueblo = Hash.new
 				for averias in 1..cantidad_averias_pueblo
 					averias -= 1
 					hash_averias = Hash.new
@@ -41,22 +42,29 @@ class AEE_API
 					hash_averias["Last Update"] = value[:return][averias][:r4_last_update]
 					hash_array_averias[averias] = hash_averias
 				end
-				hash_pueblo["#{value[:return][averias][:r1_town_or_city]}"] = hash_array_averias
+				hash_pueblo["Pueblo"] = value[:return][averias][:r1_town_or_city]
+				hash_pueblo["Averias"] = hash_array_averias
+				array_final.push hash_pueblo
 			else
 				hash_averia = Hash.new
+				hash_pueblo = Hash.new
+				array_averia = Array.new
 				hash_averia["Area"] = value[:return][:r2_area]
 				hash_averia["Statu:"] = value[:return][:r3_status]
 				hash_averia["Last Update"] = value[:return][:r4_last_update]
-				hash_pueblo["#{value[:return][:r1_town_or_city]}"] = hash_averia
+				array_averia.push hash_averia
+				hash_pueblo["Pueblo"] = value[:return][:r1_town_or_city]
+				hash_pueblo["Averias"] = array_averia
+				array_final.push hash_pueblo
 			end
 		end
-		return hash_pueblo.to_json
+		return array_final.to_json
 	end
 
 	def all_averias
 		aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
 		pueblos = Array.new
-		hash_pueblo = Hash.new
+		array_final = Array.new
 
 		aee_client = Savon.client(wsdl: aee_url)
 		breakdownSummary = aee_client.call(:get_breakdowns_summary)
@@ -76,6 +84,7 @@ class AEE_API
 			if data[:get_breakdowns_by_town_or_city_response][:return].kind_of?(Array)
 				cantidad_averias_pueblo = data[:get_breakdowns_by_town_or_city_response][:return].length
 				array_averias = Array.new
+				hash_pueblo_multi = Hash.new
 				for averias in 1..cantidad_averias_pueblo
 					averias -= 1
 					hash_averias = Hash.new
@@ -84,15 +93,22 @@ class AEE_API
 					hash_averias["Last Update"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r4_last_update]
 					array_averias[averias] = hash_averias 
 				end
-				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][averias][:r1_town_or_city]}"] = array_averias
+				hash_pueblo_multi["Pueblo"] = data[:get_breakdowns_by_town_or_city_response][:return][averias][:r1_town_or_city] 
+				hash_pueblo_multi["Averias"] = array_averias
+				array_final.push hash_pueblo_multi
 			else
 				hash_averia = Hash.new
+				array_averia = Array.new
+				hash_pueblo = Hash.new
 				hash_averia["Area"] = data[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
 				hash_averia["Status"] = data[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
 				hash_averia["Last Update"] = data[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
-				hash_pueblo["#{data[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]}"] = hash_averia
+				array_averia.push hash_averia
+				hash_pueblo["Pueblo"] = data[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]
+				hash_pueblo["Averias"] = array_averia
+				array_final.push hash_pueblo
 			end
 		end
-		return hash_pueblo.to_json
+		return array_final.to_json
 	end
 end
